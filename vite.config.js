b62a0743011ab3e36a2f9125fffffc6406650033
Harika_guntupalli <gutupalliharika@gmail.com>
2025-10-17 14:39:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-    base :"/MSD--PROJECT-",
+base: process.env.VITE_BASE_PATH || "/sample-deploy",
   
 })
 
